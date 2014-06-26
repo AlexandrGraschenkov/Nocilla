8b5b59339286047539eca24e1892f1ab9beb190d
Alexander <sharrikk@gmail.com>
2014-06-26 11:20:27 +0400

    Refactor catch all requests

--- Nocilla/Hooks/NSURLRequest/LSHTTPStubURLProtocol.m
+++ Nocilla/Hooks/NSURLRequest/LSHTTPStubURLProtocol.m
@@ -3,6 +3,7 @@
 #import "NSURLRequest+LSHTTPRequest.h"
 #import "LSStubRequest.h"
 #import "NSURLRequest+DSL.h"
+#import "LSHTTPRequestDSLRepresentation.h"
 
 @interface NSHTTPURLResponse(UndocumentedInitializer)
 - (id)initWithURL:(NSURL*)URL statusCode:(NSInteger)statusCode headerFields:(NSDictionary*)headerFields requestTime:(double)requestTime;
@@ -14,16 +15,7 @@
     if( ![@[ @"http", @"https" ] containsObject:request.URL.scheme] )
         return NO;
     
-    if([LSNocilla sharedInstance].catchAllRequests)
-        return YES;
-    
-    BOOL result = NO;
-    @try {
-        result = nil != [[LSNocilla sharedInstance] responseForRequest:request];
-    }
-    @catch (NSException *exception) { /* sometimes we don't need catch exeptions */ }
-    
-    return result;
+    return [LSNocilla sharedInstance].catchAllRequests || [[LSNocilla sharedInstance] responseForRequest:request] != nil;
 }
 
 + (NSURLRequest *)canonicalRequestForRequest:(NSURLRequest *)request {
@@ -38,6 +30,9 @@
 	id<NSURLProtocolClient> client = [self client];
 
     LSStubResponse* stubbedResponse = [[LSNocilla sharedInstance] responseForRequest:request];
+    if(stubbedResponse == nil) {
+        [NSException raise:@"NocillaUnexpectedRequest" format:@"An unexpected HTTP request was fired.\n\nUse this snippet to stub the request:\n%@\n", [[[LSHTTPRequestDSLRepresentation alloc] initWithRequest:request] description]];
+    }
 
     if (stubbedResponse.shouldFail) {
         [client URLProtocol:self didFailWithError:stubbedResponse.error];
